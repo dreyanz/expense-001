6c7b81628b3467a71465ac8ee7f4a17a14f4440d
Adrian Tabay <a.tabay@fujitsu.com>
2023-06-28 21:43:18 +0800

    feature: app module

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
+import { reducers } from './app-states/reducers';
+import { StoreModule } from '@ngrx/store';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), StoreModule.forRoot(reducers)],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
